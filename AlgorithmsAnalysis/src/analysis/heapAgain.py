3b9ae954e009d07025dab378b84113422403247d
Vendea <kbeine@live.com>
2015-10-29 18:07:35 -0500

    further work on homework, not really getting anywhere

--- AlgorithmsAnalysis/src/analysis/heapAgain.py
+++ AlgorithmsAnalysis/src/analysis/heapAgain.py
@@ -1,46 +1,27 @@
 """
 A module for doing heap stuff.
 """
-
-G1 = {'A':{'B':1, 'C':4},
-      'B':{'A':1, 'D':1},
-      'C':{'A':4, 'D':1, 'E':1},
-      'D':{'B':1, 'C':1, 'F':5},
-      'E':{'C':1, 'F':1},
-      'F':{'D':5, 'E':1}}
-test = ('v','u')
-
+ 
 class heap:
-    def __init__(self, g):
-        self.x = [None]
-        self.graph = g
-        self.values = {}
+    def __init__(self):
+        self.x = []
+        self.names = {}
         self.indices = {}
-        self.edges = [v+u for v in g for u in g[v]]
-        for v in g:
-            self.x.append(v)
-
+ 
     # return the index of the left child of the n'th element
     def left(self, n):
         return 2*n
-
+ 
     def right(self, n):
         return 2*n + 1
-
+     
     def parent(self, n):
         return n//2
-
+ 
     def heapsize(self):
         return len(self.x) - 1
-
-    def setIndices(self):
-        for v in range(0, len(self.x)):
-            self.indices[self.x[v]] = v
-
-    def setValues(self):
-        for v in self.x:
-            self.values[v]
-
+ 
+ 
     # Compare element at index n with its children
     # Swap, if necessary, with smallest, and iterate
     # on location it was swapped to.
@@ -57,11 +38,11 @@ class heap:
         self.x[n], self.x[least] = self.x[least], self.x[n]
         if least != n:
             self.reheapDown(least)
-
+ 
     def minHeapify(self):
         for i in range(self.heapsize(), 0, -1):
             self.reheapDown(i)
-
+ 
     def heapsort(self):
         sorted = [None]
         self.minHeapify()
@@ -75,16 +56,13 @@ class heap:
         self.x = sorted
 
     def insertQ(self, name, value):
-        self.x.append(name)
-        self.minHeapify()
-
-
+        self.x.append(value)
+        self.names[name] = value
+        self.heapsort()
+ 
 # When you 'run' this module, the code below will
 # automatically run, saving the keystrokes to build
 # the heap and construct the array.
-newh = heap(G1)
-#newh.x = [None, ['a', 1], ['b', 2], ['c', 3], ['d', 4]]
-newh.heapsort()
-#h = heap(None)
-#h.x = [None, 6, 9, 2, 3, 7, 4, 5, 8, 1]
-#h.heapsort()
+h = heap()
+h.x = [None, 6, 9, 2, 3, 7, 4, 5, 8, 1]
+h.heapsort()
