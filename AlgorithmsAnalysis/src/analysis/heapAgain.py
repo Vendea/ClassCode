c0ab204d61e8b9183a39e84a09eaafab1cdfc5da
Vendea <kbeine@live.com>
2015-10-30 06:21:04 -0500

    homework probably doesn't work

--- AlgorithmsAnalysis/src/analysis/heapAgain.py
+++ AlgorithmsAnalysis/src/analysis/heapAgain.py
@@ -36,6 +36,7 @@ class heap:
             if self.x[self.right(n)] < self.x[least]:
                 least = self.right(n)
         self.x[n], self.x[least] = self.x[least], self.x[n]
+        self.indices[self.x[n]], self.indices[self.x[least]] = n, least
         if least != n:
             self.reheapDown(least)
  
@@ -50,6 +51,7 @@ class heap:
             sorted.append(self.x[1])
             if self.heapsize() > 1:
                 self.x[1] = self.x.pop()
+                self.indices[self.x[1]] = 1
                 self.reheapDown(1)
             else:
                 break
@@ -58,10 +60,36 @@ class heap:
     def insertQ(self, name, value):
         self.x.append(value)
         self.names[name] = value
+        if value not in self.indices:
+            self.indices[value] = self.heapsize()-1
         self.heapsort()
 
     def decreaseKeyQ(self, name, newValue):
-        self.names[name]
+        oldval = self.names[name]
+        if newValue not in self.indices:
+            tindex = self.indices[oldval]
+            self.x[tindex] = newValue
+            self.indices[newValue] = tindex
+        self.names[name] = newValue
+        if oldval not in self.names.values():
+            self.indices.pop(oldval, None)
+        self.heapsort()
+
+    def deleteminQ(self):
+        if self.heapsize() == 0:
+            return None
+        self.heapsort()
+        rval = self.x[1]
+        newtemproot = self.x[self.heapsize()-1]
+        self.x[1] = newtemproot
+        self.indices[newtemproot] = 1
+        for k in self.names:
+            if self.names[k] == rval:
+                self.names.pop(k)
+        return rval
+        
+        
+        
  
 # When you 'run' this module, the code below will
 # automatically run, saving the keystrokes to build
