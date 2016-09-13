3fd8c238b05f269de76870ce0900f0914e458610
Vendea <kbeine@live.com>
2016-09-13 17:57:38 -0500

    RSA class 0913

---
+++ CryptoDBandNets/src/dnc/RSA.java
@@ -0,0 +1,128 @@
+package dnc;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class RSA {
+
+    static BigInteger TWO = new BigInteger("2");
+    public static void main(String[] args) {
+    	//System.out.println(factor1(new BigInteger("5612592691460157028488501867482051458587407740355339131559497670427820545466264932268319464911726122036348856881906213034456112079790167")));
+    	//System.out.println(factor2(new BigInteger("4203186347211155545407759403310287944739745105712280161537630386886194466890027616072541031930080350590569461440145611070067668431339809")));
+    	System.out.println(factor2(new BigInteger("12393740938979106085270960952777498451581397764755964574512615956658333154690831168855730082381367054663701968030817652770409915261551093300252139673702939609824212197857780837597650223306004227756966151861628501")));
+    	
+    }
+
+    /**
+     * Find the floor of the square root of a BigInteger
+     * @param n The number we wish to find the
+     * @return
+     */
+    static BigInteger BIsqrt(BigInteger n){
+        BigInteger low = BigInteger.ZERO;
+        BigInteger high = n.add(BigInteger.ONE);
+
+        while(high.subtract(low).compareTo(new BigInteger("1")) > 0){
+            BigInteger mid = low.add(high).divide(TWO);
+            BigInteger sqr = mid.multiply(mid);
+            if(sqr.compareTo(n) <= 0)
+                low = mid;
+            else
+                high = mid;
+        }
+        return low;
+    }
+    
+    /**
+     * factors a large pseudo-prime
+     * @param n the pseudo-prime to factor
+     * @return one of the factors of n
+     */
+    static BigInteger factor1(BigInteger n){
+    	BigInteger sqrt = BIsqrt(n);
+    	if(sqrt.mod(TWO).compareTo(BigInteger.ZERO) == 0){
+    		sqrt = sqrt.add(BigInteger.ONE);
+    	}
+    	while(n.mod(sqrt).compareTo(BigInteger.ZERO) != 0 ){
+    		sqrt = sqrt.add(TWO);
+    	}
+    	return sqrt;    	
+    }
+    
+    static BigInteger factor2(BigInteger n){
+    	BigInteger sqrt = BIsqrt(n);
+    	while(!isBISquare(sqrt.multiply(sqrt).subtract(n))){
+    		sqrt = sqrt.add(BigInteger.ONE);
+    	}
+    	BigInteger r = BIsqrt(sqrt.multiply(sqrt).subtract(n));
+    	return sqrt.subtract(r);
+    }
+
+    /**
+     * Determines if the given integer is a perfect square
+     * @param n The number to test
+     * @return true if the number is a perfect square
+     */
+    static boolean isBISquare(BigInteger n){
+        BigInteger sqrt = BIsqrt(n);
+        return (sqrt.multiply(sqrt).compareTo(n) == 0);
+    }
+    /**
+     * Creates a number that is the product of two primes
+     * @param numBits The desired number of bits in the product
+     * @param logDifference The desired number of bits in the difference between the primes
+     * @return A BigInteger that is the product of two primes
+     */
+    static BigInteger makeProduct(int numBits, int logDifference){
+        // Build the first factor
+        BigInteger f1 = BigInteger.ZERO.setBit(numBits / 2);
+        for(int i = 0; i < numBits/2; i++)
+            if(Math.random() < 0.5)
+                f1 = f1.setBit(i);
+        f1 = f1.nextProbablePrime();
+
+        // Build the difference
+        BigInteger diff = BigInteger.ZERO.setBit(logDifference);
+        for(int i = 0; i < logDifference/2; i++)
+            if(Math.random() < 0.5)
+                diff = diff.setBit(i);
+        BigInteger f2 = f1.add(diff).nextProbablePrime();
+        System.out.println(f1);
+        System.out.println(f2);
+
+        // Create and return their product
+        return f1.multiply(f2);
+    }
+
+    static ArrayList<Integer> factorSmooth(BigInteger n, long limit){
+        ArrayList<Integer> factors = new ArrayList<Integer>();
+
+        BigInteger bilimit = BigInteger.valueOf(limit);
+        BigInteger div = new BigInteger("3");
+        while(div.compareTo(bilimit) < 0){
+            if(n.mod(div).compareTo(BigInteger.ZERO) == 0){
+                factors.add(div.intValue());
+                n = n.divide(div);
+            } else {
+                div = div.add(TWO);
+            }
+        }
+        factors.add(n.intValue());
+        return factors;
+    }
+
+    static void fermatFactor(BigInteger n){
+        long limit = 2000;
+        BigInteger sq = BIsqrt(n);
+
+        for(int i = 1; i < 1000000; i++){
+            BigInteger target = sq.add(BigInteger.valueOf(i)).modPow(TWO, n);
+            ArrayList<Integer> facs = factorSmooth(target, limit);
+            if(facs.get(facs.size()-1) == 1){
+                System.out.println(i);
+                for(int j : facs) System.out.print(j + " ");
+                System.out.println("");
+            }
+        }
+    }
+}
