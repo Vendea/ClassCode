c0175e2df7ccb39e8b5820fa96e16cdbf9df9114
Vendea <kbeine@live.com>
2016-10-18 20:41:17 +0000

    homework

--- CryptoDBandNets/src/dnc/cryptoframe.py
+++ CryptoDBandNets/src/dnc/cryptoframe.py
@@ -96,3 +96,72 @@ def rsaEncode(p, n, e):
     return crypt
 
 
+# Takes a message M and a polynomial (bitstring) p and creates the codeword
+# x^3 + x + 1 = 0b1011
+def makeCodeword(M, p):
+    d = p << (M.bit_length() - 1)
+    M = M << (p.bit_length() - 1)
+    Morig = M
+
+    while M.bit_length() >= p.bit_length():
+        if M.bit_length() == d.bit_length():
+            M = M ^ d
+        d >>= 1
+        
+    print bin(Morig | M)
+    return Morig | M
+
+
+# Divides the polynomial (bitstring) M by the polynomial (bitstring) p
+# and produces the remainder (as a bitstring)
+def findRemainder(M, p):
+    if M.bit_length() < p.bit_length(): return M
+    d = p << (M.bit_length() - p.bit_length())
+    
+    while M.bit_length() >= p.bit_length():
+        if M.bit_length() == d.bit_length():
+            M = M ^ d
+        d >>= 1
+        
+    return M
+
+
+# Converts one of our ciphertexts to a bitstring, with error correction bits added
+# M is the text, packetSize is the network packet size, and p is the polynomial
+def createECCtext(M, packetSize, p):
+    blockSize = packetSize - p.bit_length() + 1 # Message chunk size
+    bitstring = 1
+    # Create the bit string. It will have a leading 1 which is not really part of the encoding
+    while len(M) > 0:
+        bitstring = (bitstring << 6) + code[M[0]]
+        M = M[1:]
+        
+    # pad the bit string so that it's length is a multiple of blockSize
+    numPadBits = (blockSize - ((bitstring.bit_length() - 1) % blockSize)) % blockSize
+    bitstring <<= numPadBits
+   
+    # encode one block at a time to create the encoded bitstring
+    eccBitstring = 1
+    bitmask = (1 << blockSize) - 1
+    bitmask <<= (bitstring.bit_length() - bitmask.bit_length() - 1)
+    bitstring -= (1 << (bitstring.bit_length() - 1)) # get rid of the leading 1
+    while bitmask > 0:
+        block = (bitstring & bitmask) >> (bitmask.bit_length() - blockSize)
+        block <<= (p.bit_length() - 1) # shift to make room to append check bits
+        rem = findRemainder(block, p)  # Find the check bits
+        block |= rem                   # tack on the remainder (check bits)
+        eccBitstring = (eccBitstring << packetSize) + block
+        bitmask >>= blockSize             # move the mast one block to the right
+
+    print "E:", bin(eccBitstring)
+
+# M is the encoded message, packetSize, d is the degree of the lcm polynomial
+def decodeECCtext(M, packetSize, d):
+    M = M[2:]
+    while len(M) > 0:
+        Mp = M[0:packetSize]
+        M = [packetSize:]
+        
+        
+    
+    
