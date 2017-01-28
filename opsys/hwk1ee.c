80a69e2b76ec900c7ec8181bd0f05f599952d01b
Vendea <kbeine@live.com>
2017-01-27 18:33:21 -0600

    this one is right.  old one was wrong.  shoul dhave read the assignment

--- opsys/hwk1ee.c
+++ opsys/hwk1ee.c
@@ -8,35 +8,35 @@
 #define BUFSZ 1
 
 int main(){
-	char buf;
-	int fd;
-	int nchar;
-	
-	if((fd = open("/usr/share/dict/american-english", O_RDONLY)) != -1){
-		char word[128];
-		int count = 0;
-	        int printnext = 0;
-		while((nchar = read(fd, &buf, BUFSZ)) > 0){
-			if(buf != '\n'){
-				word[count] = buf;
-				count++;
-			}
-		        else{
-		                word[count] = '\0';
-                		if(printnext){
-			                printf("%s\n", word);
-               			}
-		                printnext = 0;
-		                for(int i = 0; i < count-1; i++){
-                			if(word[i] == 'e' && word[i+1] == 'e'){
-			                        printnext = 1;
-                   			}
-		                }
-		                count = 0;
-            		}
-		}
-	}
-	else {
-		printf("error reading file\n");
-	}
+    char buf;
+    int fd;
+    int nchar;
+    
+    if((fd = open("/usr/share/dict/american-english", O_RDONLY)) != -1){
+        char word[128];
+        int count = 0;
+        int printprev = 0;
+        char store[128];
+        while((nchar = read(fd, &buf, BUFSZ)) > 0){
+            if(buf != '\n'){
+                word[count] = buf;
+                count++;
+            }
+            else{
+                 word[count] = '\0';
+                 for(int i = 0; i < count-1; i++){
+                    if(word[i] == 'e' && word[i+1] == 'e'){
+                        printf("%s\n", store);
+                    }
+                }
+                for(int i = 0; i < count+1; i++){  
+                    store[i] = word[i];
+                }
+                count = 0;
+            }
+        }
+    }
+    else {
+        printf("error reading file\n");
+    }
 }
