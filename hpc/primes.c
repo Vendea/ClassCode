16427f8b087afb9f7312a17f6fa4edcae5899634
Vendea <kbeine@live.com>
2017-02-09 13:00:53 -0600

    prime file hwk 020917

---
+++ hpc/primes.c
@@ -0,0 +1,220 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<pthread.h>
+#include<semaphore.h>
+
+/*
+ * Queue structure
+ */
+struct queue{
+  int count;            /* # items in the queue */
+  int locOfFirst;        /* index of first item  */
+  sem_t spaceSem;        /* # remaining spaces   */
+  sem_t itemsSem;        /* # items on queue     */
+  pthread_mutex_t mutex;    /* protect count        */
+  int* items;            /* array of queue ints  */
+};
+typedef struct queue queue;
+
+/*
+ * Forward declarations of some functions 
+ */
+int minNumSquares(int N);
+int isPrime(int N);
+void* addInts(void* N);
+void* findPrimes(void* s);
+void* findFourSquares(void* s);
+void addItem(queue* q, int x);
+int getItem(queue* q);
+
+/*
+ * Global variables
+ */
+int queueSize;
+queue queue0, queue1, queue2;
+
+/*
+ * Prints out all prime numbers within a range of 1000 numbers
+ *   that require four squares. The user inputs a number 
+ *   representing "where to start," and the program checks the
+ *   block of 10000 integers starting there.
+ *
+ * Two commandline parameters:
+ *   1:  Where to start
+ *   2:  Size of queues
+ */
+int main(int c, char* argv[]){
+  // Read the commandline arguments
+  int N = atoi(argv[1]);    /* Where to start */
+  queueSize = atoi(argv[2]);
+  printf("N = %d, qs = %d\n", N, queueSize);
+
+  // Allocate memory for the items arrays within the queues
+  // Your work here...
+  queue0.items = (int*) malloc(queueSize * sizeof(int));
+  queue1.items = (int*) malloc(queueSize * sizeof(int));
+  queue2.items = (int*) malloc(queueSize * sizeof(int));
+  printf("Allocated memory\n");
+
+  // Initialize the counts and locOfFirsts of the queues
+  queue0.count = queue0.locOfFirst = 0;
+  queue1.count = queue1.locOfFirst = 0;
+  queue2.count = queue2.locOfFirst = 0;
+  printf("Initialized count and loc\n");
+
+  // Initialize the queue semaphores
+  // Your work here...
+  printf("Initialized semaphores\n");
+
+  // Initialize the queue mutexes
+  // Your work here...
+  printf("Initialized mutexes\n");
+
+  // Launch the threads
+  pthread_t intsT, primesT, foursT;
+  pthread_create(&intsT, NULL, addInts, (void*)&N);
+  pthread_create(&primesT, NULL, findPrimes, NULL);
+  pthread_create(&foursT, NULL, findFourSquares, NULL);
+  printf("Launched Threads\n");
+
+   while(1){
+    int x = getItem(&queue2);
+    if(x == -1)
+      break;
+    printf("%d is prime, and needs 4 squares\n", x);
+  } 
+
+  // Join the child threads
+  // Your work here...
+
+  return 0;
+}
+
+
+/*
+ * Thread to simply add items from N to N+999 to queue0
+ */
+void* addInts(void* N){
+  int i;
+  int startValue = *((int*)N);
+  for(i = startValue; i < startValue + 1000; i++){
+    addItem(&queue0, i);
+  }
+  addItem(&queue0, -1);
+  printf("addInts done\n");
+  return NULL;
+}
+
+
+/*
+ * Thread to take items from the queue0, and place those
+ * that are prime onto queue1.
+ */
+void* findPrimes(void* s){
+  do {
+    x = getItem(&queue0); 
+    // Your work here...
+  } while(x != -1);
+
+  addItem(&queue1, -1);
+  printf("findPrimes done\n");
+}
+
+
+
+/*
+ * Thread to take primes off of queue1, and place those
+ * that require four squares onto queue2
+ */
+void* findFourSquares(void* s){
+  // Your work here...
+}
+
+
+
+/*
+ * This function adds an item x to the given queue, q.
+ *
+ * It uses the queue's mutex to protect access to the count
+ *   and to the items in the queue.
+ *
+ * Before taking an item, it waits on the space semaphore to make
+ *   sure there is room to add the item. And then it posts to the
+ *   items semaphore to announce that there is another in the queue.
+ */
+void addItem(queue* q, int x){
+  // Your work here...
+
+}
+
+
+
+/*
+ * This function removes and returns an item from the given queue, q.
+ *
+ * It uses the queue's mutex to protect access to the count and locOfFirst,
+ *   and to the items in the queue.
+ *
+ * Before adding an item, it waits on the items semaphore to make
+ *   sure there is an item to return. And then it posts to the
+ *   spaces semaphore to announce that there is another open space.
+ */
+int getItem(queue* q){
+  // Your work here...
+
+}
+
+
+
+/*
+ * Returns the minimum number of square numbers (including 0)
+ * needed to represent the input number N as the sum of those squares.
+ *
+ * By Lagrange's four squares theorem, the answer will always be <= 4.
+ */
+int minNumSquares(int N){
+  int sq = sqrt(N);
+  int a, b, c, d;        /* counters */
+  int numNotZero = 0;
+
+  for(a = 0; a <= sq; a++){
+    for(b = a; b <= sq; b++){
+      for(c = b; c <= sq; c++){
+    for(d = c; d <= sq; d++){
+      if(N == a*a + b*b + c*c + d*d)
+        goto found;
+    }
+      }
+    }
+  }
+ found:
+  if(a > 0) numNotZero++;
+  if(b > 0) numNotZero++;
+  if(c > 0) numNotZero++;
+  if(d > 0) numNotZero++;
+
+  return numNotZero;
+}
+
+
+/*
+ * Returns true if the number is a prime >= 2
+ * and false otherwise.
+ *
+ * In C, "0" means "false," "not 0" means "true".
+ */
+int isPrime(int N){
+  if(N < 2 || (N % 2) == 0)
+    return 0;
+  if(N == 2)
+    return 1;
+
+  int i;            /* counter */
+  for(i = 3; i <= sqrt(N); i = i + 2){
+    if(N % i == 0)
+      return 0;
+  }
+
+  return 1;            /* If we make it here, N has no divisors */
+}
