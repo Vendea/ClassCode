4598c23763726d5592624d0d26b06cc0a43f0bfb
Vendea <kbeine@live.com>
2017-03-09 16:25:15 -0600

    update count_parts and printing

--- opsys/wc.c
+++ opsys/wc.c
@@ -3,11 +3,21 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <ctype.h>
+
+#define BUFSIZE (16*1024)
 
 static int lines_flag = 0;
 static int words_flag = 0;
 static int bytes_flag = 0;
 static int total_flag = 0;
+
 static struct option long_options[] = 
     {
         {"lines", no_argument, 0, 'l'},
@@ -17,11 +27,24 @@ static struct option long_options[] =
         {NULL, 0, NULL, 0}
     };
 
+typedef struct count_parts{
+    long lines;
+    long words;
+    long bytes;
+} count_parts;
+
+typedef struct procinfo{
+    int pid;
+    int fdout;
+    int fileno;
+    struct procinfo *next;
+} procinfo;
+
 char *progname;
 
 void usage();
 void process_stdin();
-void process_file(char* fname);
+count_parts process_file(char* fname);
 void print_stuff(char* lines, char* words, char* bytes, char* fname);
 
 int main(int argc, char *argv[]){
@@ -75,14 +98,17 @@ int main(int argc, char *argv[]){
         process_stdin();
         print_stuff(
     }
-        if(argv[posn] == '-'){
-            process_stdin();
-        }
-        else{
-            process_file(argv[posn]);
-        }
-        posn++;
-    } while(posn < argc && nchild < )
+    else if(nfiles > 1){
+        total_flag = 1;
+    }
+    if(argv[posn] == '-'){
+        process_stdin();
+    }
+    else{
+        process_file(argv[posn]);
+    }
+    while(posn < argc && nchild < )
+    posn++;
 }
 
 void process_stdin(){
@@ -91,14 +117,51 @@ void process_stdin(){
     }
 }
 
-void process_file(char* fname){
-    while(1){} //something
+count_parts process_file(char* fname){
+    //get fd here and pass to count
+    //count_parts res = count
+    //then print
+    //print_stuff(res->lines, res->words, res->bytes, fname);
+}
+
+count_parts count(int fd){
+    char buf[BUFSIZE +1];
+    long lcount = 0;
+    long wcount = 0;
+    long bcount = 0;
+    char prev = ' ';
+
+    posix_fadvise(fd, 0, 0, POSIX_FADV_SEQUENTIAL | POSIX_FADV_NOREUSE);
+
+    int bytes_read;
+
+    while((bytes_read = read(fd, buf, BUFSIZE)) > 0){
+        char *p = buf;
+        char *end = p + bytes_read;
+
+        while(p != end){
+            if(*p == '\n'){
+                lcount ++;
+            }
+            if(isspace(*p) && (isalpha(prev) || isdigit(prev))){
+                wcount++;
+            }
+            bcount += sizeof(char);
+            prev = *p;
+            *p++;
+        }
+    }
+    count_parts *ret = (count_parts *) malloc(sizof(count_parts));
+    ret->lines = lcount;
+    ret->words = wcount;
+    ret->bytes = bcount;
+    return *ret;
 }
 
 void print_stuff(long lines, long words, long bytes, char* fname){
-    char *plines = "";
-    char *pwords = "";
-    char *pbytes = "";
+    char plines[128];
+    char pwords[128];
+    char pbytes[128];
     char tmp[128];
     int numFlags = 0;
     if(lines_flag){
@@ -116,7 +179,7 @@ void print_stuff(long lines, long words, long bytes, char* fname){
         pbytes = tmp;
         numFlags++;
     }
-    if(!numFlags){
+    if(0 == numFlags){
         snprintf(tmp, "%li", lines);
         plines = tmp;
         snprintf(tmp, "%li", words);
